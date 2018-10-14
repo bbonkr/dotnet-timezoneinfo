0805412d291311892e31110fe8cf0b02566059e7
구본철 <bbon72@idino.co.kr>
2018-10-14 17:06:53 +0900

    인수 이름 변경

--- Timezone.Sample/DateTimeOffsetExtensions.cs
+++ Timezone.Sample/DateTimeOffsetExtensions.cs
@@ -6,10 +6,16 @@ namespace Timezone.Sample
 
     public static class DateTimeOffsetExtensions
     {
-        public static DateTimeOffset ToLocalDateTime(this DateTimeOffset dateTimeOffset, string timezone)
+        /// <summary>
+        /// 지정된 시간대에 해당하는 시각을 계산합니다.
+        /// </summary>
+        /// <param name="dateTimeOffset"></param>
+        /// <param name="timeZoneId">시간대 식별자</param>
+        /// <returns></returns>
+        public static DateTimeOffset ToLocalDateTime(this DateTimeOffset dateTimeOffset, string timeZoneId)
         {
             var timeZoneInfo = TimeZoneInfo.GetSystemTimeZones()
-                .Where(t => t.Id.Equals(timezone, StringComparison.OrdinalIgnoreCase))
+                .Where(t => t.Id.Equals(timeZoneId, StringComparison.OrdinalIgnoreCase))
                 .FirstOrDefault();
 
             var localTime = TimeZoneInfo.ConvertTime(dateTimeOffset, timeZoneInfo);
